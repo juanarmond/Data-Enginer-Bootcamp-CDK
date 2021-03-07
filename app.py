6ab7887cc71f1e2d9982964ce6f950a1ca01a8cf
juanarmond <cuvviszonkax6mukTy>
2021-03-07 19:23:09 +0000

    fix bug 2

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-#!/usr/bin/env python3
-
-from aws_cdk import core as cdk
-
-# For consistency with TypeScript code, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
-
 from my_project.my_project_stack import MyProjectStack
 
 
-app = cdk.App()
+app = core.App()
 MyProjectStack(app, "my-project-CDK")
 
 app.synth()

a242342a1d5f65c2b482063ee3e41501539a5ef9
juanarmond <cuvviszonkax6mukTy>
2021-03-07 19:21:49 +0000

    fix bug 1

--- my_project/my_project_stack.py
+++ my_project/my_project_stack.py
@@ -2,8 +2,8 @@ from aws_cdk import core
 from aws_cdk import aws_s3 as s3
 
 
-class MyProjectStack(cdk.Stareck):
+class MyProjectStack(core.Stareck):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         s3.Bucket(self, 'myBucket-juanarmond-cdk', bucket_name='myBucket-juanarmond-cdk')

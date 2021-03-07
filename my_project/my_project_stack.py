e3500d965f7456412dab7ed7f7e281b35cd66622
juanarmond <cuvviszonkax6mukTy>
2021-03-07 19:40:31 +0000

    fix bug 9

--- my_project/my_project_stack.py
+++ my_project/my_project_stack.py
@@ -2,7 +2,7 @@ from aws_cdk import core
 from aws_cdk import aws_s3 as s3
 
 
-class MyProjectStack(core.Stareck):
+class MyProjectStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
